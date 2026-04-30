e0bd4edf0f04f7530642ddcd42e43ecf41e0e31c
yggdevsec <dev@yggdevsec.com>
2026-04-30 13:42:44 +0200

    while and swich init

--- FlowControl/Program.cs
+++ FlowControl/Program.cs
@@ -1,1 +1,63 @@
-﻿
+﻿using System;
+
+namespace FlowControl
+{
+    class FlowControl
+    { 
+
+        enum MenuChoice
+        {
+            Quit = 0,
+
+        }
+    
+        static void Main(String[] args)
+        {
+
+           
+
+            bool isRunning = true;
+
+            int? choice = 0;
+
+            while(isRunning)
+            {
+
+
+
+                choice = InputInt();
+
+                if (choice is null)
+                {
+                    continue;
+                }
+
+                MenuChoice numericChoice = (MenuChoice)choice;
+              
+
+                switch (numericChoice)
+                {
+                    case MenuChoice.Quit:
+                        isRunning = false;
+                        break;
+
+                    default:
+                          Console.WriteLine("Invalid choice");
+                        break;
+                }
+            }
+        }
+
+        static int? InputInt()
+        {
+            string? input = Console.ReadLine();
+            if (!int.TryParse(input, out int choice))
+            {
+                Console.WriteLine("Invalid choice. Please enter a number.");
+                return null;
+            }
+
+            return choice;
+        }
+    }
+}
